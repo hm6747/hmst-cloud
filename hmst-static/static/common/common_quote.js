460e69ee1d937fefb1e3fbeedfab94f1c3e35d75
hm6747 <674703614@qq.com>
2018-05-25 11:20:23 +0800

    更新

---
+++ hmst-static/static/common/common_quote.js
@@ -0,0 +1,62 @@
+/**
+ * 引用JS和CSS头文件
+ */
+var rootPath = getRootPath(); //项目路径
+
+/**
+ * 动态加载CSS和JS文件
+ */
+var dynamicLoading = {
+    css: function(path){
+        if(!path || path.length === 0){
+            throw new Error('argument "path" is required!');
+        }
+        document.write('<link rel="stylesheet" type="text/css" href="' + path + '">');
+    },
+    js: function(path, charset){
+        if(!path || path.length === 0){
+            throw new Error('argument "path" is required!');
+        }
+        document.write('<script charset="' + (charset ? charset : "utf-8") + '" src="' + path + '"></script>');
+    }
+};
+/**
+ * 取得项目路径
+ * @author wul
+ */
+function getRootPath() {
+    //取得当前URL
+    var path = window.document.location.href;
+    //取得主机地址后的目录
+    var pathName = window.document.location.pathname;
+    var post = path.indexOf(pathName);
+    //取得主机地址
+    var hostPath = path.substring(0, post);
+    //取得项目名
+    var name = pathName.substring(0, pathName.substr(1).indexOf("/") + 1);
+    return hostPath + name + "/";
+}
+
+//动态加载项目 JS文件
+dynamicLoading.js( "../static/js/jquery.min.js?v=2.1.4", "utf-8");
+dynamicLoading.js("../static/js/plugins/validate/jquery.validate.min.js", "utf-8");
+dynamicLoading.js("../static/common/common_base.js", "utf-8");
+dynamicLoading.js("../static/common/common-validate.js", "utf-8");
+dynamicLoading.js("../static/js/bootstrap.min.js?v=3.3.6", "utf-8");
+dynamicLoading.js("../static/js/zxf_page.js", "utf-8");
+dynamicLoading.js("../static/js/vue.js", "utf-8");
+dynamicLoading.js("../static/js/plugins/nestable/jquery.nestable.js", "utf-8");
+dynamicLoading.js("../static/js/plugins/ztree/jquery.ztree.all.min.js", "utf-8");
+
+//动态加载项目 CSS文件
+dynamicLoading.css("../static/css/bootstrap.min.css?v=3.3.6");
+dynamicLoading.css("../static/css/font-awesome.css?v=4.4.0");
+dynamicLoading.css("../static/css/plugins/iCheck/custom.css");
+dynamicLoading.css("../static/css/animate.css");
+dynamicLoading.css("../static/css/plugins/summernote/summernote.css");
+dynamicLoading.css("../static/css/plugins/fileinput/fileinput.min.css");
+dynamicLoading.css("../static/css/style.css?v=4.1.0");
+dynamicLoading.css("../static/css/custom.css");
+dynamicLoading.css("../static/js/plugins/ztree/zTreeStyle.css");
+dynamicLoading.css("../static/css/zxf_page.css");
+dynamicLoading.css("../static/css/login.css");
