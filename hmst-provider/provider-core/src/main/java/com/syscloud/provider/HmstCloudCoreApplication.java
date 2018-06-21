0d99d6de88f9edca8f4b85723680c6e3266e6ab8
hm6747 <674703614@qq.com>
2018-06-21 10:34:21 +0800

    更新

--- hmst-provider/provider-core/src/main/java/com/syscloud/provider/HmstCloudCoreApplication.java
+++ hmst-provider/provider-core/src/main/java/com/syscloud/provider/HmstCloudCoreApplication.java
@@ -1,8 +1,16 @@
 package com.syscloud.provider;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Created by Administrator on 2018/6/21 0021.
  */
+@EnableEurekaClient
+@SpringBootApplication
 public class HmstCloudCoreApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(HmstCloudCoreApplication.class, args);
+    }
 }
