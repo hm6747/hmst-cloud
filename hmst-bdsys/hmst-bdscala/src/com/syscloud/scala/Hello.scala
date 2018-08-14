2b8091ff968ecb50bdbce83afae66eea364210f0
hm6747 <674703614@qq.com>
2018-08-14 17:34:19 +0800

    更新

---
+++ hmst-bdsys/hmst-bdscala/src/com/syscloud/scala/Hello.scala
@@ -0,0 +1,11 @@
+package com.syscloud.scala
+
+/**
+  * Created by Administrator on 2018/8/14 0014.
+  */
+object Hello {
+  def main(args: Array[String]) {
+//    println("Hello")
+   println(1.to(10).map(_*10))
+  }
+}
--- hmst-zipkin/src/main/java/com/syscloud/zipkin/HmstCloudZipkinApplication.java
+++ hmst-zipkin/src/main/java/com/syscloud/zipkin/HmstCloudZipkinApplication.java
@@ -3,11 +3,9 @@ package com.syscloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.EnableZipkinServer;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableZipkinServer
 public class HmstCloudZipkinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HmstCloudZipkinApplication.class, args);
